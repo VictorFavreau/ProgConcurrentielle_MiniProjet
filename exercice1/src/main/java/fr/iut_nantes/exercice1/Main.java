2d6e0faefff428b429fee5292ad55c9760f6a59f
Victor <favreauvictor@gmail.com>
2017-11-10 16:35:36 +0100

    Correction d'un probleme d'encodage sur les noms des philosophes

--- exercice1/src/main/java/fr/iut_nantes/exercice1/Main.java
+++ exercice1/src/main/java/fr/iut_nantes/exercice1/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        final String[] noms = {"Platon", "Socrate", "Aristote", "Diogène", "Sénèque"};
+        final String[] noms = {"Platon", "Socrate", "Aristote", "Diogene", "Seneque"};
 
         table = new Philosophe[5];
         moniteur = new Moniteur();
